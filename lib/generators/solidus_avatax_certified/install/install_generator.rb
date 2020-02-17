1eabd8929e2f9a872234e97ee1b05b43554dcee8
Matteo Latini <matteolatini@nebulab.it>
2020-02-17 12:54:10 +0100

    Only require solidus_core
    Requiring solidus istead of `solidus_core` requires a ton of code that
    might not even be used in the application requiring this extension.
    
    By requiring just `solidus_core` life will be easier for people that
    are, for example, not using solidus_frontend.
    
    The new `solidus_dev_support` gem has been used in order to make the
    specs pass.

--- lib/generators/solidus_avatax_certified/install/install_generator.rb
+++ lib/generators/solidus_avatax_certified/install/install_generator.rb
@@ -3,6 +3,8 @@
 module SolidusAvataxCertified
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
+
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_avatax_certified\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_avatax_certified\n"
@@ -12,21 +14,12 @@ module SolidusAvataxCertified
         run 'bundle exec rake railties:install:migrations FROM=solidus_avatax_certified'
       end
 
-      def auto_migrate?
-        ENV['AUTO_RUN_MIGRATIONS'] =~ /true/i
-      end
-
       def run_migrations
-        # hiding this inside parent method so it's not auto-run by rails generator
-        def migration_prompt_approved?
-          result = ask('Would you like to run the migrations now? [Y/n]')
-          result !~ /n/i
-        end
-
-        if auto_migrate? || migration_prompt_approved?
-          run 'bundle exec rake db:migrate'
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        if run_migrations
+          run 'bin/rails db:migrate'
         else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+          puts 'Skipping bin/rails db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
         end
       end
 
